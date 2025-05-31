dc2ceb4c869bd96368f1dcdc44c946e282fe0974
Alla Yevlampiieva <modan1679@gmail.com>
2025-06-01 01:52:24 +0300

    Do task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,41 @@
-console.log('Form');
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', onInput);
+formEl.addEventListener('submit', onSubmit);
+
+const LOCALE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+doLoad();
+
+function onInput(evt) {
+  const { email, message } = evt.currentTarget.elements;
+  formData.email = email.value.trim();
+  formData.message = message.value.trim();
+  localStorage.setItem(LOCALE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget.elements;
+  if (!email.value || !message.value) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(LOCALE_KEY);
+  evt.currentTarget.reset();
+}
+
+function doLoad() {
+  const savedFormData = JSON.parse(localStorage.getItem(LOCALE_KEY));
+  const { email, message } = formEl;
+  if (savedFormData) {
+    email.value = savedFormData.email;
+    message.value = savedFormData.message;
+  }
+}
